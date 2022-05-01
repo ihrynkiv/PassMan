6a578bfac38b9b53a71805191ce7f2f929177ea8
Ivan Hrynkiv <Ivan.hrynkiv@gmail.com>
2022-05-01 21:36:21 +0300

    rename

---
+++ src/components/Header/Header.component.js
@@ -0,0 +1,40 @@
+import {
+  AppBar, IconButton, Toolbar, Typography,
+} from '@mui/material';
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+export function Header({ onHomeClick, onAddRecordClick }) {
+  return (
+    <AppBar position="static" color="primary" style={{ width: '396px' }} title="PassMan2">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={onHomeClick}
+        >
+          <HomeIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          PassMan
+        </Typography>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          color="inherit"
+          onClick={onAddRecordClick}
+        >
+          <AddCircleIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+

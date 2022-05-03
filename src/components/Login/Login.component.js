63f7ef468389ab0dac559967e7c60a3a9d78d890
Ivan Hrynkiv <Ivan.hrynkiv@gmail.com>
2022-05-03 20:06:51 +0300

    Apply login logic

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -1,5 +1,9 @@
-import React from 'react'
-import {AppBar, Button, IconButton, TextField, Toolbar, Typography} from "@mui/material";
+import React, {useState} from 'react'
+import {Button, TextField} from "@mui/material";
+import {useDispatch} from "react-redux";
+import {loginAction} from "../../store/auth/auth.slice";
+import {useHistory} from "react-router-dom";
+import {Toast} from "../Toast/Toast.component";
 
 const STYLES = {
   textField: { padding: '8px' },
@@ -8,8 +12,50 @@ const STYLES = {
   mainBlock: { margin: '5px' }
 }
 
+const defaultError = {
+  password: {
+    hasError: false,
+    helperText: ''
+  }
+}
 
 export const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(defaultError)
+  const [open, setOpen] = useState(false)
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const passwordValidation = (value) => {
+    if (value.length < 6) {
+      setError({...error, password: {hasError: true, helperText: 'password should contain at least 6 characters'}})
+    } else {
+      setError(defaultError)
+    }
+  }
+
+  const usernameChangeHandler = (e) => setUsername(e.target.value)
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value)
+    passwordValidation(e.target.value)
+  }
+
+  const loginClickHandler = () => {
+    passwordValidation(password)
+    if (error.password.hasError) return
+
+    const user = {username, password}
+    dispatch(loginAction(user)).then((res) => {
+      if (res.error) {
+        setOpen(true)
+      } else {
+        history.push('/')
+      }
+    })
+  }
+
   return (
     <div style={STYLES.mainBlock}>
       <TextField
@@ -18,21 +64,28 @@ export const Login = () => {
         variant="outlined"
         fullWidth
         margin="normal"
+        value={username}
+        onChange={usernameChangeHandler}
         styles={STYLES.textField}/>
       <TextField
+        error={error.password.hasError}
+        helperText={error.password.helperText}
         id="password"
         label="Password"
         variant="outlined"
         fullWidth
         type="password"
         margin="normal"
+        value={password}
+        onChange={passwordChangeHandler}
         styles={STYLES.textField}/>
       <div style={STYLES.btnBlock}>
-        <Button variant="contained" style={STYLES.btn}>
+        <Button variant="contained" style={STYLES.btn} onClick={loginClickHandler}>
           Login
         </Button>
         <Button href="/registration">Registration</Button>
       </div>
+      <Toast message="Wrong username or password" open={open} setOpen={setOpen}/>
     </div>
   )
 }
